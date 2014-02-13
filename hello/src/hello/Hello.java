3c3d488db26eebf79db4c72eb36332e45e4791ea
gunjan <gunjangugale@gmail.com>
2014-02-13 08:40:59 -0500

    added javadoc

--- hello/src/hello/Hello.java
+++ hello/src/hello/Hello.java
@@ -1,7 +1,20 @@
+/*
+ * @Author - Gunjan Gugale
+ */
+
 package hello;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Hello.
+ */
 public class Hello {
 
+	/**
+	 * Main prints two greetings.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println ("Hello.. how are you?");
